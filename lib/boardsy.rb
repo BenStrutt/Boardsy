e16e62bc4fe3ec61a85f33415ca0a08e95434422
Bennett Struttman <ben.struttman@gmail.com>
2020-11-05 05:00:20 +0000

    testing gem functionality

--- lib/boardsy.rb
+++ lib/boardsy.rb
@@ -2,5 +2,11 @@ require "boardsy/version"
 
 module Boardsy
   class Error < StandardError; end
+    
+  class Board
+    def self.testing
+      puts "Hello World!"
+    end
+  end
   # Your code goes here...
 end
